bbb079aa8d86bb801aef731f5de796fee18647b9
seongyeol <sonseong10@gmail.com>
2021-01-12 15:44:56 +0900

    Update app.js
    + btn click event

--- darkmode/app.js
+++ darkmode/app.js
@@ -1,1 +1,12 @@
-console.log("Dark MODE")
+const body = document.body
+const modeBtn = document.querySelector("#mode--btn")
+
+modeBtn.addEventListener("click", () => {
+  if (body.classList.contains("dark")) {
+    body.classList.remove("dark")
+    modeBtn.textContent = "Dark"
+  } else {
+    body.classList.add("dark")
+    modeBtn.textContent = "White"
+  }
+})
