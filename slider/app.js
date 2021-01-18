2c75f0d05862035e0338a6420aac10afe657b4ef
seongyeol <sonseong10@gmail.com>
2021-01-18 17:46:53 +0900

    Add slide function

--- slider/app.js
+++ slider/app.js
@@ -1,1 +1,23 @@
-console.log("connected")
+const VISIBLE = "visible"
+
+const firstImg = document.querySelector(".slider--img")
+
+firstImg.classList.add(VISIBLE)
+
+function autoSlide() {
+  const currentImg = document.querySelector(`.${VISIBLE}`)
+  if (currentImg) {
+    currentImg.classList.remove(VISIBLE)
+    const nextImg = currentImg.nextElementSibling
+    if (nextImg) {
+      nextImg.classList.add(VISIBLE)
+    } else {
+      firstImg.classList.add(VISIBLE)
+    }
+  } else {
+    firstImg.classList.add(VISIBLE)
+  }
+}
+
+autoSlide()
+setInterval(autoSlide, 3000)
